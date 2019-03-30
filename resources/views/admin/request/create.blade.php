dfd8f4a8bf97c29f0640e5f4a6299fd38b648108
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2019-03-29 18:52:21 -0600

    Add itemsRequest migrations and seeder

--- resources/views/admin/request/create.blade.php
+++ resources/views/admin/request/create.blade.php
@@ -6,7 +6,6 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Crear Solicitud</div>
-
                     <div class="card-body">
                         <div class="form-group row">
                             <label for="infra" class="col-md-4 col-form-label text-md-right">Infraestructura</label>
@@ -15,9 +14,10 @@
                                 <select name="infra" id="infra">
                                     <option value="">Selecciones Infraestructura</option>
                                     <option value="">Aula</option>
-                                    <option value="gym">Gimnacio</option>
+                                    <option value="gym">Gimnasio</option>
                                     <option value="library">Biblioteca</option>
                                     <option value="auditory">Auditorio</option>
+                                    <option value="auditory">Materiales</option>
                                 </select>
 
                                 @if ($errors->has('infra'))
@@ -33,15 +33,16 @@
                             <div class="col-md-6">
                                 <select name="event" id="event">
                                     <option value="">Seleccionar Evento</option>
+                                    <option value="event">Charla Scrum</option>
                                     <option value="event_tematic">Fiesta Tematica</option>
                                     <option value="event_meeting">Reuniones</option>
                                     <option value="event_study">Grupos de Estudio</option>
                                     <option value="">Fiesta de egresados</option>
                                 </select>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('event'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('event') }}</strong>
                                     </span>
                                 @endif
                             </div>
