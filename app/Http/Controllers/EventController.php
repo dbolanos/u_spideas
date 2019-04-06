c175c41338712ede77999799e8793b129f909bc4
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2019-04-06 10:41:26 -0600

    Add create Event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,7 +15,21 @@ class EventController extends Controller
     }
 
     public function create(){
+        return view('admin.events.create');
+    }
 
+    public function generateEvent(Request $request){
+        $request->validate([
+            'descrip_event' => 'required|string|max:255',
+        ]);
+
+        $new_event = Event::create(
+                [
+                    'description' => $request->descrip_event
+                ]
+        );
+        $message   = ['type_message' => 'success', 'msg' => 'Se ha creado el evento con exito! Evento #'. $new_event->id];
+        return redirect()->route('all.events')->with('message', $message);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get  ('mis-solicitudes'      , ['as' => 'my.student.requests'    , 'uses'  => 'StudentController@showRequestStudent']);
-Route::get  ('crear-solicitud-estudiante'    , ['as' => 'create.student.request'         , 'uses'  => 'RequestController@createRequestStudent']);
-Route::post ('generar-solicitud-estudiante'   , ['as' => 'generate.student.request'         , 'uses'  => 'RequestController@generateRequestStudent']);
+Route::get  ('mis-solicitudes'                  , ['as' => 'my.student.requests'        , 'uses'  => 'StudentController@showRequestStudent']);
+Route::get  ('crear-solicitud-estudiante'       , ['as' => 'create.student.request'     , 'uses'  => 'RequestController@createRequestStudent']);
+Route::post ('generar-solicitud-estudiante'     , ['as' => 'generate.student.request'   , 'uses'  => 'RequestController@generateRequestStudent']);
 
 
-Route::get  ('eventos'      , ['as' => 'all.events'    , 'uses'  => 'EventController@index']);
-Route::get  ('crear-evento'    , ['as' => 'create.event'         , 'uses'  => 'EventController@create']);
+Route::get  ('eventos'              , ['as' => 'all.events'         , 'uses'  => 'EventController@index']);
+Route::get  ('crear-evento'         , ['as' => 'create.event'       , 'uses'  => 'EventController@create']);
+Route::post ('generar-evento'       , ['as' => 'generate.event'     , 'uses'  => 'EventController@generateEvent']);
